7940a3d57c0b069b00222b155f9923319f5c22e4
Dongyeong Kim <dyeongkim98@gmail.com>
2025-04-22 22:22:44 +0900

    [FEAT] 플레이어 기본 애니메이션 구현
    어택 1 2 3
    히트
    사망
    IDLe
    무브

--- Assets/Scripts/KDY/Player.cs
+++ Assets/Scripts/KDY/Player.cs
@@ -1,12 +1,66 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour, IDamageable
 {
-    public int hp{ get; private set; }
+    private int maxHp = 100; // 최대 체력
+    public int hp{ get; private set; } // 현재 체력
+    [Tooltip("애니메이터 컴포넌트")]
+    [SerializeField] private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        hp = maxHp;
+    }
+
+    private void Update()
+    {
+        //애니메이션 테스트
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Attack1();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Attack2();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Attack3();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            TakeDamage(10);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Die();
+        }
+    }
+
+    public void Move()
+    {
+        _animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+    }
     
+    public void Attack1()
+    {
+        _animator.SetTrigger("Attack1");
+    }
+    public void Attack2()
+    {
+        _animator.SetTrigger("Attack2");
+    }
+    public void Attack3()
+    {
+        _animator.SetTrigger("Attack3");
+    }
+
     public void TakeDamage(int amount)
     {
         hp -= amount;
+        _animator.SetTrigger("TakeDamage");
         if (hp <= 0)
         {
             Die();
@@ -16,5 +70,6 @@ public class Player : MonoBehaviour, IDamageable
     public void Die()
     {
         Debug.Log("Player has died.");
+        _animator.SetTrigger("Death");
     }
 }
