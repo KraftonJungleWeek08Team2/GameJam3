10c06ff0b02b8c59b2a1fb8c7a3fc0c49105b465
kimminsuk <kminsuk1999@gmail.com>
2025-04-23 19:27:51 +0900

    [FEAT] Result_UI_controller

--- Assets/Scripts/KMS/SlotMachine.cs
+++ Assets/Scripts/KMS/SlotMachine.cs
@@ -13,6 +13,11 @@ public class SlotMachine : MonoBehaviour
     [SerializeField] private int slotCount = 3; // 슬롯의 갯수 지정
     [SerializeField] private float spinSpeed = 0.1f; // 슬롯의 숫자 돌아가는 속도 조절 가능(간격이다보니 짧을 수록 빨라집니다.)
     [SerializeField] private SlotTextGroup[] slotTextGroups; // 인스펙터에서 슬롯 개수만큼 할당
+    [SerializeField] private ResultUIManager resultUIManager; // 또 다른 유아이(결과 유아이)를 키기위한 선언
+
+    //경과 시간 추가
+    private float slotTimeout = 5f; // 제한 시간
+    private float slotTimer = 0f;   // 경과 시간
 
     public SlotInfo SlotInfo => slotInfo;
     private SlotInfo slotInfo;
@@ -41,14 +46,13 @@ public class SlotMachine : MonoBehaviour
     /// </summary>
     public void ShowSlotUI()
     {
+        slotTimer = 0f; // 슬롯 시간을 0으로 
         _slotCanvas.enabled = true;
         slotInfo = new SlotInfo(slotCount);
         displayValues = new int[slotCount, 3];
         currentSlotIndex = 0;
         isSpinning = true;
         Managers.InputManager.OnSlotEvent += ConfirmCurrentSlot;
-
-
     }
     public void HideSlotUI()
     {
@@ -70,6 +74,12 @@ public class SlotMachine : MonoBehaviour
             spinTimer = 0f;
             SpinAllUnfixedSlots();
         }
+        //제한시간과 
+        slotTimer += Time.deltaTime;
+        if (slotTimer >= slotTimeout)
+        {
+            ForceEndWithZeros(); // 제한시간 초과 시 강제 종료
+        }
     }
 
     /// <summary>
@@ -82,7 +92,7 @@ public class SlotMachine : MonoBehaviour
             // 밑으로 숫자 이동: center -> bottom, top -> center, new random -> top
             displayValues[i, 2] = displayValues[i, 1];
             displayValues[i, 1] = displayValues[i, 0];
-            displayValues[i, 0] = Random.Range(1, 10);
+            displayValues[i, 0] = Random.Range(1, 10); // 숫자 1~9까지만 나타날거유
 
             // UI 업데이트
             slotTextGroups[i].topText.text = displayValues[i, 0].ToString();
@@ -95,8 +105,9 @@ public class SlotMachine : MonoBehaviour
     /// </summary>
     void ConfirmCurrentSlot()
     {
+        if (!isSpinning) return;
         int finalValue = displayValues[currentSlotIndex, 1];
-        //Debug.Log($"{displayValues[currentSlotIndex, 1]}, {currentSlotIndex}");
+        Debug.Log($"{displayValues[currentSlotIndex, 1]}, {currentSlotIndex}");
         slotInfo.SetValue(currentSlotIndex, finalValue);
 
         // 중앙 텍스트 강조
@@ -110,16 +121,34 @@ public class SlotMachine : MonoBehaviour
             OnAllSlotsConfirmed();
         }
     }
+    /// <summary>
+    /// 5초안에 안하면 0으로 반환하는 값
+    /// </summary>
+    void ForceEndWithZeros()
+    {
+        isSpinning = false;
 
+        for (int i = currentSlotIndex; i < slotCount; i++)
+        {
+            slotInfo.SetValue(i, 0);
+            slotTextGroups[i].centerText.text = "0";
+            slotTextGroups[i].centerText.color = Color.gray;
+        }
+
+        OnAllSlotsConfirmed();
+    }
     /// <summary>
     /// 최종적으로 만들어졌고 전달해야할 슬롯의 숫자값
     /// </summary>
     void OnAllSlotsConfirmed()
     {
         //콘솔창에 확인용으로 잠깐 쓴겁니다. 지우셔도 되요.
-        int first = slotInfo.GetValue(0);
-        int second = slotInfo.GetValue(1);
-        int third = slotInfo.GetValue(2);
+        int[] result = new int[slotCount];
+        for (int i = 0; i < slotCount; i++)
+            result[i] = slotInfo.GetValue(i); 
+
+        // 전달 후
+        resultUIManager.ShowResult(result);
 
         //Debug.Log($"{first} , {second}, {third}");
 
