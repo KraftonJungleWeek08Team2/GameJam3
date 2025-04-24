c1af520a3b4e020d042ed5e031fca4a9d411fbc8
k56754665 <95170766+k56754665@users.noreply.github.com>
2025-04-24 10:24:40 +0900

    [FIX] 풀콤보 초기화 코드 추가

--- Assets/Scripts/KGJ/BattleFSM/MoveState.cs
+++ Assets/Scripts/KGJ/BattleFSM/MoveState.cs
@@ -2,6 +2,7 @@ public class MoveState : ITurnState
 {
     public void EnterState()
     {
+        Managers.TurnManager.IsFullCombo = false;
         Managers.CameraManager.ChangeBattleCamera(false); // 카메라를 MoveCamera로 변경
         Managers.TurnManager.Player.FakeRun(); // Player의 애니메이션을 FakeRun으로 변경
         Managers.TurnManager.CurrentEnemy.isMoving = true;
