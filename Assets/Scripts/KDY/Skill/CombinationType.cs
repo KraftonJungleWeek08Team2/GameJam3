4e1651271220227f386856c5fbe36966741508b8
Dongyeong Kim <dyeongkim98@gmail.com>
2025-04-25 15:42:51 +0900

    [FEAT] 조합 검사 후 스킬 발동 구현

---
+++ Assets/Scripts/KDY/Skill/CombinationType.cs
@@ -0,0 +1,8 @@
+public enum CombinationType
+{
+    ThreeOfAKind,    // 3개 동일
+    Sequential,      // 연속 수 오름차순
+    AllOdd,          // 모두 홀수
+    AllEven,         // 모두 짝수
+    Jackpot          // 777
+}
