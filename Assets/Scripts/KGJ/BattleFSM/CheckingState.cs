b0d38f57e9c2603fc442df31cc19c930e6cb45f4
k56754665 <95170766+k56754665@users.noreply.github.com>
2025-04-26 17:38:46 +0900

    [FIX] 원모어 픽스

--- Assets/Scripts/KGJ/BattleFSM/CheckingState.cs
+++ Assets/Scripts/KGJ/BattleFSM/CheckingState.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 public class CheckingState : ITurnState
 {
     // CheckingState에서 할 일 : 풀콤, 적 체력에 따라 knockback, move, slot으로 넘겨주기
@@ -28,7 +26,6 @@ public class CheckingState : ITurnState
         // 적 hp와 풀콤 여부와 슬롯 스킬 체크해서 상태 변경, onemore ui도 여기서 제어
         if (Managers.TurnManager.CurrentEnemy.hp <= 0)
         {
-            Debug.Log("[KGJ] HP <= 0");
             Managers.TurnManager.EnemyHpUI.HideEnemyUI(); // 적 체력 UI 숨기기
             Managers.CameraManager.RemoveMember(Managers.TurnManager.CurrentEnemy.transform);
             Managers.TurnManager.CurrentEnemy.Die();
@@ -45,13 +42,11 @@ public class CheckingState : ITurnState
             {
                 // 풀 콤보라면, 다시 슬롯머신 상태로
                 // 원 모어 UI를 띄움
-                Debug.Log("[KGJ] HP > 0 && FullCombo");
                 Managers.TurnManager.BeatBarPanelBehaviour.OneMoreUIBehaviour.Show();
                 Managers.TurnManager.ChangeState(new SlotState());
             }
             else
             {
-                Debug.Log("[KGJ] HP > 0 && not FullCombo");
                 // 풀 콤보가 아니라면, 플레이어가 데미지를 입고 넉백되며 다시 MoveState로 전환
                 Managers.TurnManager.ChangeState(new KnockBackState());
             }
