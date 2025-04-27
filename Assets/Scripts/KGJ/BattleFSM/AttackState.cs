14e77bfe6a6046442767658f7887b207b622344b
k56754665 <95170766+k56754665@users.noreply.github.com>
2025-04-27 15:32:53 +0900

    save

--- Assets/Scripts/KGJ/BattleFSM/AttackState.cs
+++ Assets/Scripts/KGJ/BattleFSM/AttackState.cs
@@ -1,6 +1,3 @@
-using System;
-using UnityEngine;
-
 public class AttackState : ITurnState 
 {
     SlotInfo _slotInfo;
--- Assets/Scripts/KGJ/Skills/ThreeOddSkill.cs
+++ Assets/Scripts/KGJ/Skills/ThreeOddSkill.cs
@@ -1,6 +1,3 @@
-
-using System;
-
 public class ThreeOddSkill : ISkill
 {
     int _damage;
